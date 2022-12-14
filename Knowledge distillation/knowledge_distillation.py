705f87238b781f06b94699fbda69c8d5a33bfeb7
KwonSoon Yong <45085563+reverse-sky@users.noreply.github.com>
2022-12-14 17:40:43 +0900

    Update knowledge_distillation.py

--- Knowledge distillation/knowledge_distillation.py
+++ Knowledge distillation/knowledge_distillation.py
@@ -1,8 +1,12 @@
-def knowledge_distillation_loss(logits,labels,teacher_logits):
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def knowledge_distillation_loss(teacher_logits,student_logits,labels,criterion = F.cross_entropy):
     alpha = 0.1
     T = 10
 
-    student_loss = F.cross_entropy(input=logits,target = labels)
-    distillation_loss = nn.KLDivLoss(reduction='batchmean')(F.log_softmax(logits/T, dim=1), F.softmax(teacher_logits/T, dim=1)) * (T * T)
+    student_loss = criterion(input=student_logits,target = labels)  # Calculate the loss about the Student model
+    distillation_loss = nn.KLDivLoss(reduction='batchmean')(F.log_softmax(student_logits/T, dim=1), F.softmax(teacher_logits/T, dim=1)) * (T * T)
     total_loss =  alpha*student_loss + (1-alpha)*distillation_loss
     return total_loss
