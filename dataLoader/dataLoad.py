42a4aa418fefd1caa4c7b085e163186959af06a2
KwonSoon Yong <45085563+reverse-sky@users.noreply.github.com>
2022-12-16 13:24:24 +0900

    Update dataLoad.py

--- dataLoader/dataLoad.py
+++ dataLoader/dataLoad.py
@@ -17,3 +17,39 @@ import albumentations as A
 from albumentations.pytorch import transforms 
 # import torchvision.transforms as transforms
 #albumentation을 주로 사용함
+
+class BaseDataset(Dataset):
+    def __init__(self,img_paths:str,labels = None,cache = None,use_caching = False, transform = None) -> None:
+        super().__init__()
+        self.img_paths = img_paths
+        self.transform = transform
+        self.cache = cache
+        self.labels = labels
+        self.use_caching = use_caching
+    def __len__(self)->int:
+        return len(self.img_paths)
+    
+class CacheDataset(BaseDataset):
+    def __getitem__(self,index):
+        path = self.img_paths[index]
+        image  = self.imread_cache(path)
+        if self.transform is not None:
+            # image = self.transform(image) #torchvision version
+            image = self.transform(image=image)['image'] #Albumentation ver
+        if self.labels is not None:
+            label = self.labels[index]
+            return image, label
+        else:
+            return image        
+        
+        
+    def imread_cache(self,path):
+        img = self.cache.get(path, None)  #cache에 들어있다면, 가지고 오지만 없으면 None을 return
+        if img is None:                   #cache값이 None이라면, 
+            img = cv2.imread(path)        #imread로 읽고 , cv2가 PIL보다 빠르고 tool도 더 많다.
+            # img = cv2.resize(img, (self.imsize, self.imsize))
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #RGB로 변환 
+            self.cache[path] = img
+        else:
+            pass
+        return img
